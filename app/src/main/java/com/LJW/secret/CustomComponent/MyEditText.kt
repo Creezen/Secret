e87fd95d81b79acd952335cffca15b6c49aebe5d
LJW <ljw5786165598@163.com>
2023-09-02 15:51:26 +0800

    secret demo commit

---
+++ app/src/main/java/com/LJW/secret/CustomComponent/MyEditText.kt
@@ -0,0 +1,8 @@
+package com.LJW.secret.CustomComponent
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEditText(context: Context): AppCompatEditText(context) {
+
+}
---
+++ app/src/main/java/com/LJW/secret/Utils.kt
@@ -0,0 +1,6 @@
+package com.LJW.secret
+
+import android.view.View
+import android.widget.TextView
+
+fun getTextviewMessage(view: TextView)=view.text.toString()
